b5445b16f4a0e1940a3225cc2d4cc8803677bb7c
FarVal <imcoolfer@gmail.com>
2025-10-09 11:57:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1. pilih tombol bedasarkan ID-nya
-const tombolSapa = document.getElementById('sapabutton')
+const tombolSapa = document.getElementById('sapabutton');
 //2. Tambahkan 'event litsener' untuk aksi 'click'
 tombolSapa.addEventLitsener('click',function() {
   alert('what'sup?');

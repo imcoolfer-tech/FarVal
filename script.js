980d2ef78789fbab11a6e06792c6e6640289a983
FarVal <imcoolfer@gmail.com>
2025-10-11 21:13:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombolSapa = document.getElementById('sapabutton');
 
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
-  alert("What's up?");
+  alert("+62 815-1547-7975");
 });

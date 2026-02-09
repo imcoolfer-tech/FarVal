658a5f24d5a1db130cb121ae95d08a7ab187451e
FarVal <imcoolfer@gmail.com>
2026-02-09 13:32:51 +0700

    Change alert message on button click

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombolSapa = document.getElementById('sapabutton');
 
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
-  alert("NOAH HAS A GIRLFRIEND GUYSS!!!");
+  alert("kyah didik~");
 });
